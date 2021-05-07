9cc1db28da9f72f4cc4c71842961d88adc5ae152
jmendozacr <mendoza077@gmail.com>
2021-05-07 16:27:32 -0600

    adding structure of the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Home = () => {
+    const imageName = 'classic-carousel.jpg';
+
+    const getImage = (image) => {
+        return <img src={require(`./../images/${image}`).default} />
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
-            <p>hello world</p>
+        <div className="row">
+            <div className="col-sm-8">
+                <section className="container-image">
+                    {getImage(imageName)}
+                </section>
+            </div>
+            <div className="col-sm-4">
+                <aside>
+                    <h1>Choose Your Mattress</h1>
+                    <p>description</p>
+                </aside>
+            </div>
         </div>
     );
 }
