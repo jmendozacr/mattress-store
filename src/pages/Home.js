b2073ee5bbdf5f94485e66b98f018d6a9e323ebd
jmendozacr <mendoza077@gmail.com>
2021-05-07 17:34:59 -0600

    adding home styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     const imageName = 'classic-carousel.jpg';
 
     const getImage = (image) => {
-        return <img src={require(`./../images/${image}`).default} />
+        return <img src={require(`./../images/${image}`).default} alt="text"/>
     }
 
     return (
@@ -17,7 +17,19 @@ const Home = () => {
             <div className="col-sm-4">
                 <aside>
                     <h1>Choose Your Mattress</h1>
-                    <p>description</p>
+                    <p className="letter-spacing">SELECT MATTRESS TYPE</p>
+                    <div className="btn-group btn-group-md container-type-buttons" role="group" aria-label="Button type">
+                        <button type="button" className="btn active">Saatva Classic</button>
+                        <button type="button" className="btn">Loom & Leaf</button>
+                        <button type="button" className="btn">Zenhaven</button>
+                    </div>
+                    <div className="container-price">
+                        <p>Name Mattress</p>
+                        <p>$2000</p>
+                    </div>
+                    <button className="btn btn-add">
+                        Add to Cart
+                    </button>
                 </aside>
             </div>
         </div>
