c7f4c015ffbd7bfd5595300f7e08463030630907
Josue Mendoza <josuem@thehangar.cr>
2021-05-07 22:54:40 -0600

    adding onAdd method and setActiveButton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import data from './../api/mattresses.json';
+import Button from './../components/button/Button';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
     const [selectedProduct, setSelectedProduct] = useState(products[0]);
+    const [active, setActive] = useState(0);
+    const [cart, setCart] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
     const getImage = (image) => {
         return <img src={require(`./../images/${image}`).default} alt="text"/>
@@ -11,13 +15,31 @@ const Home = () => {
 
     const getProducts = () => {
         const { mattresses } = data;
-        const splitObject = Object.keys(mattresses).map(value => {
-            return {...mattresses[value], type: value};
-        });
 
-        console.log("splitObject", splitObject);
-        setSelectedProduct(splitObject[0]);
-        return setProducts(products => [...products, ...splitObject]);;
+        setProducts(
+            Object.keys(mattresses).map(value => {
+                return {...mattresses[value], type: value}
+            })
+        );
+    }
+
+    const setActiveProduct = (item, index) => {
+        setActive(index);
+        setSelectedProduct(item);
+    }
+
+    const onAddProduct = (product) => {
+        const exist = cart.find(item => item.type === product.type);
+
+        if(exist) {
+            setCart(
+                cart.map(item => 
+                    item.type === product.type ? {...exist, quantity: exist.quantity + 1} : item
+                )
+            );
+        } else {
+            setCart([...cart, { ...product, quantity: 1 }]);
+        }
     }
 
     useEffect(() => {
@@ -28,7 +50,7 @@ const Home = () => {
         <div className="row">
             <div className="col-sm-8">
                 <section className="container-image">
-                    { selectedProduct && getImage(selectedProduct.imageFileName)}
+                    { selectedProduct ? getImage(selectedProduct.imageFileName) : ''}
                 </section>
             </div>
             <div className="col-sm-4">
@@ -38,23 +60,23 @@ const Home = () => {
                     <div className="btn-group btn-group-md container-type-buttons" data-toggle="button" role="group" aria-label="Button type">
                         {
                             products.length ?
-                            products.map(item => (
-                                <button
-                                    key={item.type} 
-                                    onClick={() => setSelectedProduct(item)} 
-                                    type="button" 
-                                    className={`btn`}>
-                                        {item.name}
-                                </button>
+                            products.map((item, index) => (
+                                <Button
+                                    key={index}
+                                    item={item}
+                                    index={index}
+                                    active={active}
+                                    setActiveProduct={setActiveProduct}
+                                />
                             ))
                             : <></>
                         }
                     </div>
                     <div className="container-price">
                         { selectedProduct && <p>{selectedProduct.name} Mattress</p> }
-                        { selectedProduct && <p>${selectedProduct.price}</p> }
+                        { selectedProduct && <p>${selectedProduct.price.toFixed(2)}</p> }
                     </div>
-                    <button className="btn btn-add">
+                    <button onClick={() => onAddProduct(selectedProduct)} className="btn btn-add">
                         Add to Cart
                     </button>
                 </aside>
@@ -63,4 +85,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default React.memo(Home);
