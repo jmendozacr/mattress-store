60ec857f3ede6b09bf8e10bb26f6f131aafe3174
Josue Mendoza <josuem@thehangar.cr>
2021-05-08 13:23:13 -0600

    adding delete icon

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useMattressesContext } from '../../context/mattressesContext';
+import { ReactComponent as DeleteIcon } from './../../images/delete.svg';
 
 const Product = ({ product }) => {
-    // ./../images/${product.imageFileName}
+    const { removeProduct } = useMattressesContext();
+
     return (
         <li>
             <img src={require(`./../../images/${product.imageFileName}`).default} alt="product"/>
             <div className="d-flex copy">
                 <p>{product.name} x {product.quantity}</p>
                 <p className="price"> <strong>${product.price.toFixed(2)}</strong></p>
+                <button onClick={() => removeProduct(product.type)} className="btn btn-danger"><DeleteIcon/></button>
             </div>
         </li>
     );
